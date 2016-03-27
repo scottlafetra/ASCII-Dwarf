3660fabcd648ac69a450f85672e9eecc7e369511
Sygh <Flores.f.victor@gmail.com>
2016-03-27 02:09:08 -0700

    Input Draft v2
    Cleaned Up Code. Still needs to be hooked up.  Gooing to merge back into
    master.

--- ASCII-Dwarf/main.cpp
+++ ASCII-Dwarf/main.cpp
@@ -1,16 +1,5 @@
-#include "InputHandler.h"
-
-InputHandler * handler;
-
 int main(void)
 {
-	handler = InputHandler::GetHandler();
-	cout << "Hello World" << endl;
 
-	printf("\x1b[30;1m");
-
-	handler->Run();
-
-	system("pause");
 	return 0;
 }
